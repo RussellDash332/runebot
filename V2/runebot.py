28b4ad68f90a2813db727a65d3d5e9a7beb6242d
RussellDash332 <russellsaerang@gmail.com>
2021-09-13 00:17:11 +0800

    Add Image.open() method

--- V2/runebot.py
+++ V2/runebot.py
@@ -176,6 +176,7 @@ def hollusion_rune(update, context):
                 bio.name = f"hollusion_{update.message.chat['id']}.gif" # dummy name?
                 bio = save_hollusion(bio, bio.name)
                 bio.seek(0)
+                bio = Image.open(bio)
                 clear_all()
                 try:
                     update.message.reply_animation(bio, reply_to_message_id = msg_id)
