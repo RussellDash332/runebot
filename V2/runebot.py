4c90d9c59cfe800964b4073ed6cd35ef3dc2643e
RussellDash332 <russellsaerang@gmail.com>
2021-09-13 03:57:34 +0800

    Add global scope

--- V2/runebot.py
+++ V2/runebot.py
@@ -6,7 +6,7 @@ secret_runes = ['MOSAIC', 'SIMPLE_FRACTAL', 'EGYPTIAN',
                 'FRACTAL', 'DUAL_FRACTAL', 'STEPS',
                 'TREE', 'HELIX']
 for rune in secret_runes:
-        exec(os.environ[rune])
+        exec(os.environ[rune], globals())
 
 from runes import *
 
